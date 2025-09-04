0b1af32b2c7f01f733c7dab786d8b2db06bec337
Wasif-Ansari <wasifansari2709@gmail.com>
2025-09-04 22:50:44 +0530

    Update

--- components/AdminComponents/Sidebar.jsx
+++ components/AdminComponents/Sidebar.jsx
@@ -14,6 +14,12 @@ const Sidebar = () => {
       </div>
 
       {/* Menu Items */}
+      <Link href="/" className='w-full group mb-3'>
+        <div className='w-full flex items-center gap-3 p-4 rounded-lg transition duration-300 ease-in-out hover:bg-fuchsia-600 hover:bg-opacity-50 hover:shadow-neon cursor-pointer'>
+          <Image src={assets.arrow} alt='icon' width={24} height={24} className="rotate-180 filter invert group-hover:filter-none transition duration-300 ease-in-out group-hover:drop-shadow-[0_0_8px_rgba(232,121,249,0.9)]" />
+          <span className='text-gray-200 group-hover:text-white transition duration-300 ease-in-out group-hover:drop-shadow-[0_0_8px_rgba(232,121,249,0.9)]'>Home</span>
+        </div>
+      </Link>
       <Link href="/admin/addproduct" className='w-full group'>
         <div className='w-full flex items-center gap-3 p-4 rounded-lg transition duration-300 ease-in-out hover:bg-fuchsia-600 hover:bg-opacity-50 hover:shadow-neon cursor-pointer'>
           <Image src={assets.add_icon} alt='icon' width={24} height={24} className="filter invert group-hover:filter-none transition duration-300 ease-in-out group-hover:drop-shadow-[0_0_8px_rgba(232,121,249,0.9)]" />
